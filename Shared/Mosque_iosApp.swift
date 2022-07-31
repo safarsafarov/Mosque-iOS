e045e94cef5fb1c4dd634ca5b2acc24d21eb5f15
Safar Safarv <safarsafarov@MacBook-Pro-Safar.local>
2022-07-31 15:09:28 +0500

    first commmit

---
+++ Shared/Mosque_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mosque_iosApp.swift
+//  Shared
+//
+//  Created by Safar Safarv on 31/07/22.
+//
+
+import SwiftUI
+
+@main
+struct Mosque_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
