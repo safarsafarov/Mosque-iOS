20b79efe78e23811a94a6b8a4581ec7849765b6b
Safar Safarov <Safarov.safar@hotmail.com>
2022-08-01 13:39:21 +0500

    ✨

---
+++ Mosque-App/Mosque-App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Mosque-App
+//
+//  Created by Safar Safarov on 03/08/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Mosque-App/Mosque-App/Mosque_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mosque_AppApp.swift
+//  Mosque-App
+//
+//  Created by Safar Safarov on 03/08/22.
+//
+
+import SwiftUI
+
+@main
+struct Mosque_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
