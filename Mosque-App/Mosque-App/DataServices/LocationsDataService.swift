9f051a586ee8d4cbde4fdd3b08fdf59ef1a70a84
Safar Safarov <Safarov.safar@hotmail.com>
2022-08-01 13:46:22 +0500

    ✨

---
+++ Mosque-App/Mosque-App/DataServices/LocationsDataService.swift
@@ -0,0 +1,20 @@
+//
+//  LocationsDataService.swift
+//  Mosque-App
+//
+//  Created by Safar Safarov on 03/08/22.
+//
+
+import SwiftUI
+
+struct LocationsDataService: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LocationsDataService_Previews: PreviewProvider {
+    static var previews: some View {
+        LocationsDataService()
+    }
+}
