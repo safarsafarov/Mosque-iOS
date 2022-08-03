e65aecc5ed8ea08504ad1fe34892ab32eef2684d
Safar Safarov <Safarov.safar@hotmail.com>
2022-08-03 11:57:06 +0500

    ✨

---
+++ Mosque-App/Mosque-App/Models/Location.swift
@@ -0,0 +1,20 @@
+//
+//  Location.swift
+//  Mosque-App
+//
+//  Created by Safar Safarov on 03/08/22.
+//
+
+import SwiftUI
+
+struct Location: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Location_Previews: PreviewProvider {
+    static var previews: some View {
+        Location()
+    }
+}
---
+++ Mosque-App/Mosque-App/Views/LocationsListView.swift
@@ -0,0 +1,20 @@
+//
+//  LocationsListView.swift
+//  Mosque-App
+//
+//  Created by Safar Safarov on 03/08/22.
+//
+
+import SwiftUI
+
+struct LocationsListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LocationsListView_Previews: PreviewProvider {
+    static var previews: some View {
+        LocationsListView()
+    }
+}
